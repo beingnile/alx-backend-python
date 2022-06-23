74bc26cd540227d9857cf7dbf664a8e86eab5a9c
Nile Okomo <okomonile3@gmail.com>
2022-06-23 16:42:36 +0300

    Define type-annotated function floor that returns the floor of passed argument

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+"""Defines a type-annotated function floor
+"""
+
+
+def floor(n: float) -> int:
+    """Gets the floor of n
+
+    Args:
+        n: The float to find the floor of
+
+    Returns:
+        The floor of n
+    """
+
+    return (int(n))
