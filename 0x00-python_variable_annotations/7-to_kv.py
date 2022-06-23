42440e500edb19ad9ddab98cacc547ced0fa434e
Nile Okomo <okomonile3@gmail.com>
2022-06-23 19:48:55 +0300

    Define type-annotated function to_kv that returns a tuple of the passed arguments

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+
+"""Defines a type-annotated function to_kv and returns a tuple of the values
+"""
+
+
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Takes a string k and an int OR float v
+    as arguments and returns a tuple
+
+    Args:
+        k: A string
+        v: Either an int or tuple
+
+    Returns:
+        A tuple containing a string and a square of v
+    """
+
+    return((k, float(v) ** 2))
