9893a3ff343fff8df8cea00c825bccad5175f6ac
Nile Okomo <okomonile3@gmail.com>
2022-06-23 15:34:14 +0300

    Define type-annotated function add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+"""Defines a type-annotated function add
+"""
+
+
+def add(a: float, b: float) -> float:
+    """Sums up a and b
+
+    Args:
+        a: First argument to be summed
+        b: Second argument
+
+    Returns:
+        The sum of arguments a and b
+    """
+    return(a + b)
