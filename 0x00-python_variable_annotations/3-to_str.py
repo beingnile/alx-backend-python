c62c2d4e3bfedd00ca5fa1e5fc524066d392e65c
Nile Okomo <okomonile3@gmail.com>
2022-06-23 17:03:09 +0300

    Define function to_str that converts a float to a string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+
+"""Defines a type-annotated function to_str
+"""
+
+
+def to_str(n: float) -> str:
+    """Converts a float to a string
+
+    Args:
+        n: Float argument to be converted
+
+    Returns:
+        String representation of n
+    """
+
+    return(str(n))
