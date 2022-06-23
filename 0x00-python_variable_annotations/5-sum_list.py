85f18089c572853c0b2a18ab33ad615f694cdec8
Nile Okomo <okomonile3@gmail.com>
2022-06-23 18:06:30 +0300

    Sum values in a list

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+
+"""Defines a type-annotated function sum_list that returns the sum
+of values in a list
+"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Sums up elements in a list
+
+    Args:
+        input_list: A list of float values
+
+    Returns:
+        The sum of float values in input_list
+    """
+    res = 0
+
+    for val in input_list:
+        res += val
+
+    return(res)
