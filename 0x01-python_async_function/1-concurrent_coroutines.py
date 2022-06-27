dbb1d9b250a22cabb70993782992dddd2deb142c
Nile Okomo <okomonile3@gmail.com>
2022-06-28 01:57:57 +0300

    Move module level import to top of file

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,10 +3,10 @@
 """Defines an asynchronous coroutine wait_n
 """
 
+from typing import List
 import asyncio
 import random
 wait_random = __import__('0-basic_async_syntax').wait_random
-from typing import List
 
 
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
