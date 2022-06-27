3790beb0e39377c6fc9ce6e40b858424ee37365c
Nile Okomo <okomonile3@gmail.com>
2022-06-28 02:35:54 +0300

    Return an asyncio.Task object

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""Defines an asynchronous coroutine wait_n
+"""
+
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+def task_wait_random(max_delay: int) -> float:
+    """Creates a task
+
+    Args:
+        max_delay: Maximum delay value
+    Returns:
+        Asyncio.Task object
+    """
+    task = asyncio.create_task(wait_random(max_delay))
+
+    return task
